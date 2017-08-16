15824d8a61afcaa8f1bfc63569cef1a78e448b14
Michelle Dick <michelle.dick@nutanix.com>
2017-08-15 21:24:04 -0700

    More nitpicking.

--- src/views/BookView.js
+++ src/views/BookView.js
@@ -10,20 +10,26 @@ class BookView extends Component {
     super(props);
     console.log(this.props)
     // need to make sure this location shim shame actually exists
-    if(this.props.location.state) {
-      this.state = {book:this.props.location.state.book }
-    }
-    else {
-      this.state = {book:{name:'dunno', publish_date:'prehistory', subjects:'subjective',
-      title:'god it all broke',
-      openlibrary_medcover_url: "../images/lincoln-inaug-bible.jpg"
-}}
+    if (this.props.location.state) {
+      this.state = {
+        book: this.props.location.state.book
+      }
+    } else {
+      this.state = {
+        book: {
+          name: 'dunno',
+          publish_date: 'prehistory',
+          subjects: 'subjective',
+          title: 'god it all broke',
+          openlibrary_medcover_url: "../images/lincoln-inaug-bible.jpg"
+        }
+      }
     }
 
   }
   render(target) {
     console.log(this)
-    let booksie = this.state.book;
+    let book = this.state.book;
     return (
       <div className="singleBookDiv">
         <div className="singleBookDesc">
@@ -35,19 +41,19 @@ class BookView extends Component {
           </div>
           <div>
             <span className="bookDescLabel">Published: </span>
-            {booksie.publish_date}
+            {book.publish_date}
           </div>
           <div>
             <span className="bookDescLabel">Subjects: </span>
-            {booksie.subjects}
+            {book.subjects}
           </div>
           <div>
             <span className="bookDescLabel">Page Count: </span>
-            {booksie.number_of_pages}
+            {book.number_of_pages}
           </div>
         </div>
         <div className="singleBookCoverDiv">
-          <img src={booksie.openlibrary_medcover_url} alt="Book Image">
+          <img src={book.openlibrary_medcover_url} alt="Book Image">
           </img>
         </div>
       </div>
